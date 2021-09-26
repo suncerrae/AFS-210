fa44c89468ff8fde2673c8696330ed8e7d659dcd
sun <sun.quinones@bryanuniversity.edu>
2021-09-26 13:35:05 -0400

    x

---
+++ week3/week4/hashing.py
@@ -0,0 +1,43 @@
+class HashTable:
+    def __init__(self) -> None:
+        self.size = 10
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def hashfunction(self,key):
+        # Insert your hashing function code
+        keystr = str(key)
+        hashVal = 0
+        for ch in str(keystr):
+            hashVal += ord(ch)
+            
+        return (hashVal % self.size)
+
+    def rehash(self,key):
+        # Insert your secondary hashing function code
+        return
+
+    def put(self,key,data):
+        # Insert your code here to store key and data 
+        return
+
+    def get(self,key):
+        # Insert your code here to get data by key
+        return
+
+    def __getitem__ (self,key):
+        return self.get(key)
+
+    def __setitem__ (self,key,data):
+        self.put(key,data)
+
+
+H = HashTable()
+H[69] = 'A'
+# Store remaining input data
+
+# print the slot values
+
+# print the data values
+
+# print the value for key 52
---
+++ week4/hashing.py
@@ -0,0 +1,18 @@
+int_val = 4
+str_val = 'PythonIsBest'
+flt_val = 24.56
+
+
+  
+# print(hash(4))  
+                                                                                                  
+
+# print(hash(str_val))    
+
+# print(hash(flt_val))  
+
+print("The integer hash value is : " + str(hash(4)))
+
+print("The string hash value is : " + str(hash(str_val)))
+
+print("The float hash value is : " + str(hash(flt_val)))
